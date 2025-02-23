4061a0da225ee6d650be527a94e83a26863215dc
Pablo Velez <pablovelezlopera3@gmail.com>
2025-02-22 20:31:42 -0500

    Ejercicio-02-Variables

---
+++ Ejercicio-02-Variables/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        String queEs = "Perro";
+        String queEs1 = "Gato";
+
+        String nombre = "Manchitas";
+        String nombre1 = "Silvestre";
+
+        Integer edad = 2;
+        Integer edad1 = 3;
+
+        Double come = 1.5;
+        Double come1 = 0.5;
+
+        String sonido = "Gua gua";
+        String sonido1 = "miau miau";
+
+        System.out.println(nombre + " Tiene " + edad + " años.");
+        System.out.println(nombre + " come " + come + " kilos y hace " + sonido);
+
+        System.out.println(nombre1 + " Tiene " + edad1 + " años.");
+        System.out.println(nombre1 + " come " + come1 + " kilos y hace " + sonido1);
+
+    }
+}
